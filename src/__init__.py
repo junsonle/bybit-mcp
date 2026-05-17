bd9b56e961435fd32dae4554c96350facac309bf
le_on <52043563+junsonle@users.noreply.github.com>
2026-05-17 22:51:11 +0700

    Refactor FastMCP initialization with environment variables

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,8 @@
 from mcp.server.fastmcp import FastMCP
+import os
 
-mcp = FastMCP("Bybit MCP Server")
+# mcp = FastMCP("Bybit MCP Server")
+
+port = int(os.environ.get("PORT", 8000))
+host = os.environ.get("FASTMCP_HOST", "0.0.0.0")
+mcp = FastMCP("Bybit MCP Server", host=host, port=port)
